a0ece6d8904fbdc12137f774cff858495cbd0d7e
MarceloFigueira <marcelofigueirapereira@gmail.com>
2017-10-18 15:30:29 -0300

    criado model Category e populado com categorias usando o seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)C
+
+puts "Cadastrando as categorias....."
+
+categories = [ "Animais e acessórios",
+                "Esportes",
+                "Para a sua casa",
+                "Eletrônicos e celulares",
+                "Músicas e Hobbies",
+                "Bebes e crianças",
+                "Moda e beleza",
+                "Veículos e barcos",
+                "Imóveis",
+                "Empregos e negócios" ]
+
+categories.each do |category|
+  Category.find_or_create_by(description: category)
+end
+puts "Categorias cadastradas com sucesso"
